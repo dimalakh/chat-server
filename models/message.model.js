b722c876ebdef1262abdfc144f9438779ec15d8d
dimalakh <dmytrolakh@gmail.com>
2017-05-14 20:13:55 +0300

    rewrote get messages by conv id with populate

--- models/message.model.js
+++ models/message.model.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose');
 
 const messageSchema = mongoose.Schema({
     sender: {
-        type: mongoose.Schema.ObjectId
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
     },
     msg: {
         type: String,
         required: true
     },
     date: { 
-        type: Date,
-        default: Date.now
+        type: Number,
+        default: Date.now()
     }
 });
 
--- routes/chat.router.js
+++ routes/chat.router.js
@@ -5,82 +5,96 @@ const User = require('../models/user.model');
 const Message = require('../models/message.model');
 const Conversation = require('../models/conversation.model');
 
-router.get('/conversations', (req, res) => {
-    Conversation.find().exec((err, conversations) => {
-        if (err) res.send(err);
-        res.status(200).json(conversations);
-    });
-});
 
-router.post('/conversation', (req, res) => {
-    const newConversation = new Conversation ({});
-    newConversation.save((err, data) => {
-        if (err) res.send(err);
-        User.findOne({_id: '5916f46068ad8f07a2472b03'})
-        .exec((err, user) => {
-            user.conversations.push(newConversation);
-            user.save();
-        });
-        res.status(200).json(data);
-    });
-});
-
-router.post('/message', (req, res) => {
-    const newMessage = new Message ({
-        msg: req.body.msg
-    });
-
-    newMessage.save((err, message) => {
-        if (err) res.send(err);
-        res.status(200).json(message);
-    });
-});
 
 //get user conversations
 router.get('/:userId', (req, res) => {
     User.findOne({ _id: req.params.userId})
-        .exec((err, user) => {
-            Conversation.find({ _id: {$in: user.conversations}})
-            .exec((err, conversation) => {
-                res.send(conversation);
-            })
+    .exec((err, user) => {
+        Conversation.find({ _id: {$in: user.conversations}})
+        .exec((err, conversation) => {
+            res.send(conversation);
         });
+    });
 });
 
 // get  messages by conversation id
 router.get('/conversation/:id', (req, res) => {
     Conversation.findOne({ _id: req.params.id})
-        .exec((err, conversation) => {
-            Message.find({ _id: {$in: conversation.messages }})
-            .exec((err, messages) => {
-                res.send(messages);
-            });
-        });
-});
-
-router.get('/message/:id', (req, res) => {
-    Message.findOne({ _id: req.params.id })
-        .exec((err, message) => {
-            if (err) res.send(err);
-            res.status(200).json(message);
-        });
-});
-
-router.delete('/message/:id', (req, res) => {
-    Message.findByIdAndRemove({ _id: req.params.id })
-        .exec((err) => {
-            if (err) res.send(err);
-            res.status(200).send(200);
-        });
-});
-
-router.put('/message/:id', (req, res) => {
-    const data = { msg:req.body.msg };
-    Message.findOneAndUpdate({ _id: req.params.id }, data)
-        .exec((err, message) => {
-            if(err) res.send(err);
-            res.status(200).send('message updated');
+    .populate('messages')
+    .exec((err, conversation) => {
+        Message.find({ _id: {$in: conversation.messages }})
+        .populate({
+            path: 'sender',
+            select: 'username'
         })
+        .exec((err, messages) => {
+            res.send(messages);
+        });
+    });
+    // .exec((err, conversation) => {
+    //     Message.find({ _id: {$in: conversation.messages }})
+    //     .populate('sender')
+    //     .exec((err, messages) => {
+    //         res.send(messages);
+    //     });
+    // });
 });
+
+// router.get('/conversations', (req, res) => {
+//     Conversation.find().exec((err, conversations) => {
+//         if (err) res.send(err);
+//         res.status(200).json(conversations);
+//     });
+// });
+
+// router.post('/conversation', (req, res) => {
+//     const newConversation = new Conversation ({});
+//     newConversation.save((err, data) => {
+//         if (err) res.send(err);
+//         User.findOne({_id: '5916f46068ad8f07a2472b03'})
+//         .exec((err, user) => {
+//             user.conversations.push(newConversation);
+//             user.save();
+//         });
+//         res.status(200).json(data);
+//     });
+// });
+
+// router.post('/message', (req, res) => {
+//     const newMessage = new Message ({
+//         msg: req.body.msg
+//     });
+
+//     newMessage.save((err, message) => {
+//         if (err) res.send(err);
+//         res.status(200).json(message);
+//     });
+// });
+
+// router.get('/message/:id', (req, res) => {
+//     Message.findOne({ _id: req.params.id })
+//         .exec((err, message) => {
+//             if (err) res.send(err);
+//             res.status(200).json(message);
+//         });
+// });
+
+// router.delete('/message/:id', (req, res) => {
+//     Message.findByIdAndRemove({ _id: req.params.id })
+//         .exec((err) => {
+//             if (err) res.send(err);
+//             res.status(200).send(200);
+//         });
+// });
+
+// router.put('/message/:id', (req, res) => {
+//     const data = { msg:req.body.msg };
+//     Message.findOneAndUpdate({ _id: req.params.id }, data)
+//         .exec((err, message) => {
+//             if(err) res.send(err);
+//             res.status(200).send('message updated');
+//         })
+// });
 
 module.exports = router;
